7177813f76f081a5cddc8dc325b35acd39bb60da
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-11-21 23:35:33 -0700

    Update 1_HelloWorld.java

--- 1_HelloWorld.java
+++ 1_HelloWorld.java
@@ -1,10 +1,7 @@
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+        {
+            System.out.println("Hello, World");
+        }
+ }
