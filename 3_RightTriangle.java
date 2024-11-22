3722a255ba3ca4abdf07e110d5afdce812d450de
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2024-11-21 23:37:33 -0700

    Update 3_RightTriangle.java

--- 3_RightTriangle.java
+++ 3_RightTriangle.java
@@ -1,21 +1,20 @@
-public class RightTriangle{
-    public static void main(String[] args) {
+public class RightTriangle 
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int aa = Math.min(a, Math.max(b,c));
+        int cc = Math.max(a,Math.max(b,c));
+        int bb= a + b + c - aa - cc;
+        boolean result = (aa > 0 && bb > 0 && cc > 0) && (aa * aa + bb * bb == cc * cc);
+        System.out.println(result) ;
+
+    }
 
 
-                //converts arguments into integers for sure 
-                int a = Integer.parseInt(args[0]);
-                int b = Integer.parseInt(args[1]);
-                int c = Integer.parseInt(args[2]);
-
-                int smallest = Math.min(a, Math.max(b,c));
-                int largest = Math.max(a,Math.min(b,c));
-                int middle= a+b+c -largest - smallest;
-
-                System.out.println(smallest * smallest + middle * middle == largest * largest) ;
-
-                }
-
-
-    }  //end main
+}  
 
 
