de650feed6f87569cb431ec5b078f50f159fdc12
powdered_bread <104197490+powderedbread@users.noreply.github.com>
2025-01-02 20:47:05 -0700

    Create 013_easyPrint.java

---
+++ 013_easyPrint.java
@@ -0,0 +1,15 @@
+  // prints anything basic in string format
+
+    // non static 
+    private void print(Object text)
+    {
+        String converted = text.toString();
+        System.out.println(converted);
+    }
+
+  //static
+    private static void print(Object text)
+    {
+        String converted = text.toString();
+        System.out.println(converted);
+    }
